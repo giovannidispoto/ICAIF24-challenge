a46ca1adb39e6fa437dfb634e588b7e7cb13b446
lorecampa <lorenzo.campana@mail.polimi.it>
2024-11-03 10:41:00 +0100

    add online_rl agents

---
+++ agent/dqn.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+from agent.base import AgentBase
+from sbx import DQN
+import os
+from collections import Counter
+
+class AgentDQN(AgentBase):
+    def __init__(
+        self,
+        model_path: str,
+    ):
+        seeds_dir = os.listdir(model_path)
+        print(f'Loading {len(seeds_dir)} seeds, {seeds_dir}')
+        self.agents = [DQN.load(os.path.join(model_path, seed_dir)) for seed_dir in seeds_dir]
+        self.device = torch.device("cpu")
+
+    def action(
+        self,
+        state: np.ndarray,
+    ):  
+        tensor_state = torch.as_tensor(state, dtype=torch.float32, device=self.device)
+        actions = [agent.predict(tensor_state, deterministic=True)[0][0] for agent in self.agents]        
+        return Counter(actions).most_common(1)[0][0]
--- agent/factory.py
+++ agent/factory.py
@@ -1,5 +1,7 @@
 import os
+from agent.dqn import AgentDQN
 from agent.fqi import AgentFQI
+from agent.ppo import AgentPPO
 PROJECT_FOLDER = "./"
 
 AGENTS_FOLDER = os.path.join(PROJECT_FOLDER, "agents")
@@ -12,5 +14,11 @@ class AgentsFactory:
         if agent_info['type'] == 'fqi':
             policy_path = os.path.join(AGENTS_FOLDER, agent_info['file'])
             return AgentFQI(policy_path)
+        elif agent_info['type'] in ['dqn', 'ppo']:
+            model_path = os.path.join(AGENTS_FOLDER, agent_info['file'])
+            if agent_info['type'] == 'dqn':
+                return AgentDQN(model_path)
+            elif agent_info['type'] == 'ppo':
+                return AgentPPO(model_path)
         else:
             raise NotImplementedError
